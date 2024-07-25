f1239e47df594f7cc90b7fd3898b26dc63c4b5d1
William Mattingly <wjbmattingly@gmail.com>
2024-07-24 21:24:28 -0400

    fixed pages

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1721868644',resources: [
+    cacheName: 'chirpy-1721870656',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
